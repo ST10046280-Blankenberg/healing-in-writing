a79880170dffd8fcb2d3e0f422f39079503e0fcf
ST10257863 <ST10257863@vcconnect.edu.za>
2025-10-17 16:52:46 +0200

    Added Registration Domain Model with Data Annotations

--- Domain/Events/Registration.cs
+++ Domain/Events/Registration.cs
@@ -1,7 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HealingInWriting.Domain.Events;
 
 // TODO: Represent an attendee registration value object protecting invariants.
 public class Registration
 {
-    // TODO: Include attendee identity, contact data, and RSVP state.
+    [Key]
+    public int RegistrationId { get; set; }         // PK: registration_id
+
+    [Required]
+    public int EventId { get; set; }                // FK: event_id
+
+    [Required]
+    public int UserId { get; set; }                 // FK: user_id
+
+    [Required]
+    public DateTime RegistrationDate { get; set; }  // registration_date
+
+    // TODO: Navigation properties for related entities
+    // public Event Event { get; set; }
+    // public User User { get; set; }
 }
