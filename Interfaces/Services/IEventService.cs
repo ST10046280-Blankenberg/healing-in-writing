dc2fac0049d7ecd6ad0b538bc5b938803dd9fe6c
ST10257863 <ST10257863@vcconnect.edu.za>
2025-10-20 13:42:12 +0200

    Feature: Added CalculateDistanceKm to IEventService

--- Interfaces/Services/IEventService.cs
+++ Interfaces/Services/IEventService.cs
@@ -4,4 +4,6 @@ namespace HealingInWriting.Interfaces.Services;
 public interface IEventService
 {
     // TODO: Add scheduling, detail retrieval, and RSVP coordination methods.
+    public double CalculateDistanceKm(double lat1, double lon1, double lat2, double lon2);
+
 }
