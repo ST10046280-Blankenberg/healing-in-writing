8bf3b6a1ceb8071ca62d29c1fefdce307156aaf0
ST10257863 <ST10257863@vcconnect.edu.za>
2025-10-20 17:35:42 +0200

    Feature: Implement basic CRUD operations in IStoryRepository

--- Repositories/Stories/StoryRepository.cs
+++ Repositories/Stories/StoryRepository.cs
@@ -1,3 +1,4 @@
+using HealingInWriting.Domain.Stories;
 using HealingInWriting.Interfaces.Repository;
 
 namespace HealingInWriting.Repositories.Stories;
@@ -6,4 +7,28 @@ namespace HealingInWriting.Repositories.Stories;
 public class StoryRepository : IStoryRepository
 {
     // TODO: Inject DbContext and map story aggregates to database entities.
+    public Task AddAsync(Story story)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int storyId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Story>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Story?> GetByIdAsync(int storyId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Story story)
+    {
+        throw new NotImplementedException();
+    }
 }
