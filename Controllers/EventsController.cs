e51592506e9988008597f878977ac3af26149e10
Luke Petzer <luke.petzer@icloud.com>
2025-10-21 10:40:34 +0200

    feat: basic implementation of the design, need to make changes to the event cards for better scaling

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -5,6 +5,10 @@ namespace HealingInWriting.Controllers
     // TODO: Depend on an events service for scheduling and registration logic.
     public class EventsController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
         // TODO: Implement thin actions that coordinate event workflows via the service.
     }
 }
