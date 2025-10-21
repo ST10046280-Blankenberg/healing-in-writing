07e6b32ee5162981b017431ddf195d459eece34f
ST10046280-Blankenberg <st10046280@vcconnect.edu.za>
2025-10-21 15:38:21 +0200

    refactor(stories): convert index to use story list view model

--- Controllers/StoriesController.cs
+++ Controllers/StoriesController.cs
@@ -1,4 +1,5 @@
 using HealingInWriting.Interfaces.Services;
+using HealingInWriting.Models.Stories;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -16,7 +17,21 @@ namespace HealingInWriting.Controllers
         public async Task<IActionResult> Index()
         {
             var stories = await _storyService.GetPublishedAsync();
-            return View(stories);
+
+            var viewModel = new StoryListViewModel
+            {
+                Stories = stories.Select(story => new StorySummaryViewModel
+                {
+                    StoryId = story.StoryId,
+                    Title = story.Title,
+                    Summary = story.Summary,
+                    CreatedAt = story.CreatedAt,
+                    AuthorName = story.Author?.UserId ?? string.Empty,
+                    Tags = story.Tags.Select(tag => tag.Name).ToList()
+                }).ToList()
+            };
+
+            return View(viewModel);
         }
     }
 }
