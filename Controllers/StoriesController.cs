4988aa99233270d1a816072f867cf6a783356576
Luke Petzer <luke.petzer@icloud.com>
2025-10-21 08:59:43 +0200

    feat: implemented the design for stories page, with partial views for maintainability, added some basic logic for story card population.

--- Controllers/StoriesController.cs
+++ Controllers/StoriesController.cs
@@ -5,6 +5,13 @@ namespace HealingInWriting.Controllers
     // TODO: Inject a stories service to encapsulate storytelling business rules.
     public class StoriesController : Controller
     {
+        // GET: Stories/Index
+        public IActionResult Index()
+        {
+            // TODO: Replace with actual story data from service/repository
+            return View();
+        }
+        
         // TODO: Keep actions minimal, delegating to stories service methods.
     }
 }
