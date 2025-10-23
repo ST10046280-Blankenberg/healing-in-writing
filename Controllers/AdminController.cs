f1024bf3858f89cb403d131416e7946d4298f788
ST10257863 <ST10257863@vcconnect.edu.za>
2025-10-23 08:03:33 +0200

    Feature: Added Role Authorization to AdminController. Added admin tab to _Layouts added links to admin pages in _AdminLayout

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,10 +1,52 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HealingInWriting.Controllers
 {
     // TODO: Inject an admin application service to handle privileged operations.
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         // TODO: Add actions that defer management tasks to the admin service layer.
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult ManageStories()
+        {
+            return View();
+        }
+
+        public IActionResult ManageEvents()
+        {
+            return View();
+        }
+
+        public IActionResult ManageBooks()
+        {
+            return View();
+        }
+
+        public IActionResult ApproveHours()
+        {
+            return View();
+        }
+
+        public IActionResult Reports()
+        {
+            return View();
+        }
+
+        public IActionResult SiteSettings()
+        {
+            return View();
+        }
     }
 }
