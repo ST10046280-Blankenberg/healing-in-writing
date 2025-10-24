3fd848955d0a6921fe49429e5d910aa902ce8b8b
ST10257863 <ST10257863@vcconnect.edu.za>
2025-10-24 14:46:52 +0200

    Feature: ISBN title now dynamically populates based in the length of the value entered. Categories are added as tags to allow submission of multiple categories.
    TODO: Condition and price need to be correctly implemented. Categories should allow the user to select from categories that already exist

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,3 +1,4 @@
+using HealingInWriting.Domain.Books;
 using HealingInWriting.Interfaces.Services;
 using HealingInWriting.Models.Books;
 using Microsoft.AspNetCore.Authorization;
@@ -101,5 +102,36 @@ namespace HealingInWriting.Controllers
 
             return Json(new { success = true, data = viewModel });
         }
+
+        //    [HttpPost]
+        //    [Authorize(Roles = "Admin")]
+        //    public async Task<IActionResult> AddBook(IFormCollection form)
+        //    {
+        //        var isbns = new List<string>();
+        //        if (!string.IsNullOrWhiteSpace(form["IsbnPrimary"])) isbns.Add(form["IsbnPrimary"]);
+        //        if (!string.IsNullOrWhiteSpace(form["IsbnSecondary"])) isbns.Add(form["IsbnSecondary"]);
+
+        //        var book = new Book
+        //        {
+        //            Title = form["Title"],
+        //            Authors = form["Author"].ToString().Split(',').Select(a => a.Trim()).ToList(),
+        //            Publisher = form["Publisher"],
+        //            PublishedDate = form["PublishDate"],
+        //            Description = form["Description"],
+        //            PageCount = int.TryParse(form["PageCount"], out var pc) ? pc : 0,
+        //            Categories = form["Categories"].ToString().Split(',').Select(c => c.Trim()).Where(c => !string.IsNullOrWhiteSpace(c)).ToList(),
+        //            Language = form["Language"],
+        //            IndustryIdentifiers = isbns.Select(isbn => new IndustryIdentifier
+        //            {
+        //                Type = isbn.Length == 13 ? "ISBN_13" : "ISBN_10",
+        //                Identifier = isbn.Trim()
+        //            }).ToList(),
+        //            // TODO: Handle ImageLinks, PreviewLink, InfoLink as needed
+        //        };
+
+        //        // TODO: Save book to database or in-memory collection
+
+        //        return RedirectToAction("ManageBooks");
+        //    }
     }
 }
