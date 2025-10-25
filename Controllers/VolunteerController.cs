370a7ab7d7ea9e3e4310869962417fc5a645dc8a
Luke Petzer <luke.petzer@icloud.com>
2025-10-25 13:41:22 +0200

    feat: implement volunteer dashboard layout with and added the login for the button navigation.

--- Controllers/VolunteerController.cs
+++ Controllers/VolunteerController.cs
@@ -5,6 +5,24 @@ namespace HealingInWriting.Controllers
     // TODO: Inject a volunteer service that governs enrolment and scheduling.
     public class VolunteerController : Controller
     {
+        // GET: /Volunteer/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        // GET: /Volunteer/LogHours
+        public IActionResult LogHours()
+        {
+            return View();
+        }
+        
+        // GET: /Volunteer/MyEvents
+        public IActionResult MyEvents()
+        {
+            return View();
+        }
+        
         // TODO: Expose volunteer endpoints that just orchestrate calls to the service.
     }
 }
