36872ed5dff9b001aaf1221a6f762d3e6fb18f65
ST10257863 <ST10257863@vcconnect.edu.za>
2025-10-28 17:01:54 +0200

    Chore: Organised and added XML notation to IBookService and BookService

--- Services/Books/BookService.cs
+++ Services/Books/BookService.cs
@@ -20,130 +20,123 @@ public class BookService : IBookService
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly ILogger<BookService> _logger;
 
-    //1. List of 100 ISBNs to seed
+    #region Seed Data
+
+    /// <summary>
+    /// List of 100 ISBNs to seed the database.
+    /// </summary>
     private static readonly List<string> SeedIsbns = new()
-{
-    "9780765326355", // The Way of Kings
-    "9780399590504", // Educated
-    "9780735211292", // Atomic Habits
-    "9780062316097", // Sapiens
-    "9780143127741", // The Martian
-    "9781501124020", // The Power of Habit
-    "9780062457738", // The Subtle Art of Not Giving a F*ck
-    "9780307271037", // The Road
-    "9780385472579", // Zen and the Art of Motorcycle Maintenance
-    "9780553380163", // A Short History of Nearly Everything
-    "9780439023528", // The Hunger Games
-    "9780439358071", // Harry Potter and the Order of the Phoenix
-    "9780439554930", // Harry Potter and the Sorcerer's Stone
-    "9780316769488", // The Catcher in the Rye
-    "9780743273565", // The Great Gatsby
-    "9780451524935", // 1984
-    "9780393007442", // The Odyssey
-    "9780142437230", // Moby-Dick
-    "9780156012195", // The Little Prince
-    "9780141182803", // Brave New World
-    "9780143039433", // The Kite Runner
-    "9780307949486", // Ready Player One
-    "9780307887443", // The Girl on the Train
-    "9780061122415", // To Kill a Mockingbird
-    "9780307588371", // Gone Girl
-    "9780007155668", // The Alchemist
-    "9780141033570", // Thinking, Fast and Slow
-    "9780307474278", // The Help
-    "9780143110439", // Eat, Pray, Love
-    "9780375704024", // Beloved
-    "0747549559", // Harry Potter and the Philosopher’s Stone
-    "9780765376671", // Words of Radiance
-    "9780590353427", // Harry Potter and the Chamber of Secrets
-    "9780590353403", // Harry Potter and the Prisoner of Azkaban
-    "9780553579901", // A Game of Thrones
-    "9780553573428", // A Clash of Kings
-    "9780553801507", // A Storm of Swords
-    "9780307588364", // Inferno
-    "9780307959478", // Origin
-    "9780671027360", // Angels & Demons
-    "9781400031702", // The Da Vinci Code
-    "9781400079987", // The Lost Symbol
-    "9780307277671", // The Night Circus
-    "9780307387899", // The Shack
-    "9780307588388", // Digital Fortress
-    "9780446310789", // Mockingjay
-    "9780679783268", // Pride and Prejudice
-    "9780486282114", // Dracula
-    "9780141439600", // Jane Eyre
-    "9780141442464", // Wuthering Heights
-    "9780140177398", // Of Mice and Men
-    "9780143127550", // All the Light We Cannot See
-    "9780385533224", // The Goldfinch
-    "9780307743657", // The Silent Patient
-    "9781250301697", // Where the Crawdads Sing
-    "9781501161938", // It Ends with Us
-    "9780062870605", // The Tattooist of Auschwitz
-    "9780316420259", // The Institute
-    "9780385545968", // The Midnight Library
-    "9780525559474", // The Testaments
-    "9781982137274", // The Invisible Life of Addie LaRue
-    "9780593139135", // Project Hail Mary
-    "9780593318172", // Klara and the Sun
-    "9780345803481", // Fifty Shades of Grey
-    "9780307346612", // The Road Back to You
-    "9780310344513", // Love Does
-    "9780062409851", // The Magnolia Story
-    "9780316499019", // The Vanishing Half
-    "9780735224315", // Becoming
-    "9781982137120", // The Boy, The Mole, The Fox and The Horse
-    "9780735222359", // The Four Winds
-    "9780306824203", // Educated (alternate)
-    "9780062976583", // Greenlights
-    "9780593230251", // The Body Keeps the Score
-    "9780062856625", // The Outsider
-    "9780062963675", // The Institute
-    "9781984801258", // Circe
-    "9780316556347", // Strange the Dreamer
-    "9781501160771", // The Woman in the Window
-    "9780140283334", // The Tipping Point
-    "9780062315009", // Outliers
-    "9781594206115", // Grit
-    "9780525536291", // 12 Rules for Life
-    "9780349411903", // You Are a Badass
-    "9780140449266", // Crime and Punishment
-    "9780140447934", // The Brothers Karamazov
-    "9780140449198", // War and Peace
-    "9780143039433", // The Kite Runner
-    "1338878956", // Harry Potter and the Goblet of Fire
-    "9780439136365", // Harry Potter and the Half-Blood Prince
-    "9780545010221", // Harry Potter and the Deathly Hallows
-    "9780385754728", // The Book Thief
-    "9780064400550", // Bridge to Terabithia
-    "9780064404992", // Charlotte's Web
-    "9780140366913", // Matilda
-    "014240733X", // The Outsiders
-    "9780375826696", // A Wrinkle in Time
-    "9780307474278", // The Help
-    "9780142407333", // The Outsiders (alternate)
-    "9780307277671", // The Night Circus
-    "9781451673319", // Fahrenheit 451
-    "9780307743657"  // The Silent Patient (duplicate allowed for real datasets, but can be replaced)
-};
-
-    public BookService(
-        IBookRepository bookRepository,
-        IConfiguration configuration,
-        IHttpClientFactory httpClientFactory,
-        ILogger<BookService> logger)
     {
-        _bookRepository = bookRepository;
-        _configuration = configuration;
-        _httpClientFactory = httpClientFactory;
-        _logger = logger;
-    }
+        "9780765326355", // The Way of Kings
+        "9780399590504", // Educated
+        "9780735211292", // Atomic Habits
+        "9780062316097", // Sapiens
+        "9780143127741", // The Martian
+        "9781501124020", // The Power of Habit
+        "9780062457738", // The Subtle Art of Not Giving a F*ck
+        "9780307271037", // The Road
+        "9780385472579", // Zen and the Art of Motorcycle Maintenance
+        "9780553380163", // A Short History of Nearly Everything
+        "9780439023528", // The Hunger Games
+        "9780439358071", // Harry Potter and the Order of the Phoenix
+        "9780439554930", // Harry Potter and the Sorcerer's Stone
+        "9780316769488", // The Catcher in the Rye
+        "9780743273565", // The Great Gatsby
+        "9780451524935", // 1984
+        "9780393007442", // The Odyssey
+        "9780142437230", // Moby-Dick
+        "9780156012195", // The Little Prince
+        "9780141182803", // Brave New World
+        "9780143039433", // The Kite Runner
+        "9780307949486", // Ready Player One
+        "9780307887443", // The Girl on the Train
+        "9780061122415", // To Kill a Mockingbird
+        "9780307588371", // Gone Girl
+        "9780007155668", // The Alchemist
+        "9780141033570", // Thinking, Fast and Slow
+        "9780307474278", // The Help
+        "9780143110439", // Eat, Pray, Love
+        "9780375704024", // Beloved
+        "0747549559", // Harry Potter and the Philosopher’s Stone
+        "9780765376671", // Words of Radiance
+        "9780590353427", // Harry Potter and the Chamber of Secrets
+        "9780590353403", // Harry Potter and the Prisoner of Azkaban
+        "9780553579901", // A Game of Thrones
+        "9780553573428", // A Clash of Kings
+        "9780553801507", // A Storm of Swords
+        "9780307588364", // Inferno
+        "9780307959478", // Origin
+        "9780671027360", // Angels & Demons
+        "9781400031702", // The Da Vinci Code
+        "9781400079987", // The Lost Symbol
+        "9780307277671", // The Night Circus
+        "9780307387899", // The Shack
+        "9780307588388", // Digital Fortress
+        "9780446310789", // Mockingjay
+        "9780679783268", // Pride and Prejudice
+        "9780486282114", // Dracula
+        "9780141439600", // Jane Eyre
+        "9780141442464", // Wuthering Heights
+        "9780140177398", // Of Mice and Men
+        "9780143127550", // All the Light We Cannot See
+        "9780385533224", // The Goldfinch
+        "9780307743657", // The Silent Patient
+        "9781250301697", // Where the Crawdads Sing
+        "9781501161938", // It Ends with Us
+        "9780062870605", // The Tattooist of Auschwitz
+        "9780316420259", // The Institute
+        "9780385545968", // The Midnight Library
+        "9780525559474", // The Testaments
+        "9781982137274", // The Invisible Life of Addie LaRue
+        "9780593139135", // Project Hail Mary
+        "9780593318172", // Klara and the Sun
+        "9780345803481", // Fifty Shades of Grey
+        "9780307346612", // The Road Back to You
+        "9780310344513", // Love Does
+        "9780062409851", // The Magnolia Story
+        "9780316499019", // The Vanishing Half
+        "9780735224315", // Becoming
+        "9781982137120", // The Boy, The Mole, The Fox and The Horse
+        "9780735222359", // The Four Winds
+        "9780306824203", // Educated (alternate)
+        "9780062976583", // Greenlights
+        "9780593230251", // The Body Keeps the Score
+        "9780062856625", // The Outsider
+        "9780062963675", // The Institute
+        "9781984801258", // Circe
+        "9780316556347", // Strange the Dreamer
+        "9781501160771", // The Woman in the Window
+        "9780140283334", // The Tipping Point
+        "9780062315009", // Outliers
+        "9781594206115", // Grit
+        "9780525536291", // 12 Rules for Life
+        "9780349411903", // You Are a Badass
+        "9780140449266", // Crime and Punishment
+        "9780140447934", // The Brothers Karamazov
+        "9780140449198", // War and Peace
+        "9780143039433", // The Kite Runner
+        "1338878956", // Harry Potter and the Goblet of Fire
+        "9780439136365", // Harry Potter and the Half-Blood Prince
+        "9780545010221", // Harry Potter and the Deathly Hallows
+        "9780385754728", // The Book Thief
+        "9780064400550", // Bridge to Terabithia
+        "9780064404992", // Charlotte's Web
+        "9780140366913", // Matilda
+        "014240733X", // The Outsiders
+        "9780375826696", // A Wrinkle in Time
+        "9780307474278", // The Help
+        "9780142407333", // The Outsiders (alternate)
+        "9780307277671", // The Night Circus
+        "9781451673319", // Fahrenheit 451
+        "9780307743657"  // The Silent Patient (duplicate allowed for real datasets, but can be replaced)
+    };
 
-    // Seed books into the database if not already present
+    /// <summary>
+    /// Seeds books into the database if not already present.
+    /// </summary>
     public async Task SeedBooksAsync()
     {
         var existingBooks = (await _bookRepository.GetAllAsync()).ToList();
-        // Pull every stored ISBN so we avoid hitting Google Books for titles that already exist locally.
         var existingIsbns = existingBooks
             .SelectMany(b => b.IndustryIdentifiers ?? new List<IndustryIdentifier>())
             .Select(id => id.Identifier)
@@ -168,13 +161,42 @@ public class BookService : IBookService
         }
     }
 
+    #endregion
+
+    #region Constructor
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BookService"/> class.
+    /// </summary>
+    public BookService(
+        IBookRepository bookRepository,
+        IConfiguration configuration,
+        IHttpClientFactory httpClientFactory,
+        ILogger<BookService> logger)
+    {
+        _bookRepository = bookRepository;
+        _configuration = configuration;
+        _httpClientFactory = httpClientFactory;
+        _logger = logger;
+    }
+
+    #endregion
+
+    #region Book Retrieval
+
+    /// <inheritdoc />
     public async Task<IReadOnlyCollection<Book>> GetFeaturedAsync()
     {
         var books = await _bookRepository.GetAllAsync();
         return books.ToList();
     }
 
-    public async Task<IReadOnlyCollection<Book>> GetFeaturedFilteredAsync(string searchTerm, string selectedAuthor, string selectedCategory, string selectedTag)
+    /// <inheritdoc />
+    public async Task<IReadOnlyCollection<Book>> GetFeaturedFilteredAsync(
+        string searchTerm,
+        string selectedAuthor,
+        string selectedCategory,
+        string selectedTag)
     {
         var books = await GetFeaturedAsync();
 
@@ -194,6 +216,17 @@ public class BookService : IBookService
         return books.ToList();
     }
 
+    /// <inheritdoc />
+    public Task<Book?> GetBookByIdAsync(int id)
+    {
+        return _bookRepository.GetByIdAsync(id);
+    }
+
+    #endregion
+
+    #region Book Import
+
+    /// <inheritdoc />
     public async Task<Book?> ImportBookByIsbnAsync(string isbn)
     {
         if (string.IsNullOrWhiteSpace(isbn))
@@ -238,12 +271,11 @@ public class BookService : IBookService
         return book;
     }
 
-    public BookDetailViewModel ToBookDetailViewModel(Book book)
-        => book.ToBookDetailViewModel();
+    #endregion
 
-    public Book ToBookFromDetailViewModel(BookDetailViewModel model)
-        => model.ToBookFromDetailViewModel();
+    #region Book CRUD
 
+    /// <inheritdoc />
     public async Task<(bool Success, string? ErrorMessage)> AddBookFromFormAsync(IFormCollection form)
     {
         try
@@ -260,6 +292,7 @@ public class BookService : IBookService
         }
     }
 
+    /// <inheritdoc />
     public async Task<bool> DeleteBookAsync(int id)
     {
         var book = await _bookRepository.GetByIdAsync(id);
@@ -270,21 +303,29 @@ public class BookService : IBookService
         return true;
     }
 
+    /// <inheritdoc />
     public async Task UpdateBookAsync(Book book)
     {
         await _bookRepository.UpdateAsync(book);
     }
 
-    public Task<Book?> GetBookByIdAsync(int id)
-    {
-        return _bookRepository.GetByIdAsync(id);
-    }
+    #endregion
 
-    // New: Map a list of Book to a list of BookSummaryViewModel
+    #region Mapping Methods
+
+    /// <inheritdoc />
+    public BookDetailViewModel ToBookDetailViewModel(Book book)
+        => book.ToBookDetailViewModel();
+
+    /// <inheritdoc />
+    public Book ToBookFromDetailViewModel(BookDetailViewModel model)
+        => model.ToBookFromDetailViewModel();
+
+    /// <inheritdoc />
     public List<BookSummaryViewModel> ToBookSummaryViewModels(IEnumerable<Book> books)
         => books.Select(ViewModelMappers.ToBookSummaryViewModel).ToList();
 
-    // New: Build a BookListViewModel from a list of books
+    /// <inheritdoc />
     public BookListViewModel ToBookListViewModel(IEnumerable<Book> books)
     {
         var summaries = ToBookSummaryViewModels(books);
@@ -301,4 +342,6 @@ public class BookService : IBookService
                                       .ToList()
         };
     }
+
+    #endregion
 }
