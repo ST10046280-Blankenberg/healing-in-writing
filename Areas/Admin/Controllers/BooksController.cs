f017dc64e7a6bd94fa2e32ec476d1c251f90cc26
ST10257863 <ST10257863@vcconnect.edu.za>
2025-10-29 09:28:49 +0200

    Feature: EditBooks now pulls the condition statuses

--- Areas/Admin/Controllers/BooksController.cs
+++ Areas/Admin/Controllers/BooksController.cs
@@ -85,6 +85,14 @@ namespace HealingInWriting.Areas.Admin.Controllers
         [HttpGet]
         public async Task<IActionResult> EditBook(int id)
         {
+            ViewBag.BookConditions = Enum.GetValues(typeof(BookCondition))
+            .Cast<BookCondition>()
+            .Select(bc => new SelectListItem
+            {
+                Value = bc.ToString(),
+                Text = bc.ToString()
+            }).ToList();
+
             var book = await _bookService.GetBookByIdAsync(id);
             if (book == null)
                 return NotFound();
